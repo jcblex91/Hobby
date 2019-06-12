b3d6ff2e1157eebe718a2e3b1e128c75b916d6d6
Jacob <reachjacobalex@gmail.com>
2019-06-13 02:31:20 +0530

    keyword generator complete

---
+++ tag_generator/keyword_analysis.py
@@ -0,0 +1,42 @@
+from newspaper import Article
+from textblob import TextBlob
+import paralleldots
+from collections import Counter
+
+# Parallel Dots API Config
+paralleldots.set_api_key( "YOUR API KEY" )
+
+
+url ='https://www.tripwire.com/state-of-security/featured/what-rfid-skimming'
+article = Article(url)
+article.download()
+article.parse()
+text = article.text
+
+desired_list=['rfid','payment','pos','frequency','identification','attack','security','debit','credit','contact']
+keywords=[]
+all_keywords=[]
+
+blob = TextBlob(text)
+
+# print(blob.noun_phrases)
+for sentence in blob.sentences:
+    # print(sentence)
+    line =TextBlob(str(sentence))
+    for tag in line.noun_phrases:
+        all_keywords.append(str(tag))
+
+# Delete strings from a list which do not contain certain words
+keywords= [x for x in all_keywords if any(word in x for word in desired_list)]
+
+#
+#
+# # Sort List Based on Occurances
+# keywords.sort(key=Counter(keywords).get, reverse=True)
+
+# Remove Repeatitions
+keywords=list(set(keywords))
+
+print(keywords)
+
+
